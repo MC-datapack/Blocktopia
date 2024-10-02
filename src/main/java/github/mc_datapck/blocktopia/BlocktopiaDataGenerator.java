ce9b1f902ee498dddb5a409d12d0032b38f9e11a
MC-datapack <timo.tewes@web.de>
2024-10-02 13:36:00 +0200

    Changed Workflow

---
+++ src/main/java/github/mc_datapck/blocktopia/BlocktopiaDataGenerator.java
@@ -0,0 +1,11 @@
+package github.mc_datapck.blocktopia;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BlocktopiaDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
